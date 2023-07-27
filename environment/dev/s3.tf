8f89544a58c275de1a59f57c8bcf560463a04f18
Karthik Shanmugam <koundykarthik@gmail.com>
2023-07-27 17:25:21 +0530

    Changed bucket

--- environment/dev/s3.tf
+++ environment/dev/s3.tf
@@ -1,5 +1,5 @@
 module "s3_karthik" {
   source            = "../../module/s3"
-  bucket_name       = "Karthiktestbucketrandom12345"
+  bucket_name       = "karthik7846839q8q36896"
   aws_s3_bucket_acl = "private"
 }
