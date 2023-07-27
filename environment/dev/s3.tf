16a475bf7b8efbf29d28a0206077a4f6e0cf8c9b
koundykarthik <koundykarthik@gmail.com>
2023-07-27 12:05:48 +0530

    Update s3.tf

--- environment/dev/s3.tf
+++ environment/dev/s3.tf
@@ -1,5 +1,5 @@
 module "s3_karthik" {
   source            = "../../module/s3"
-  bucket_name       = "Karthiktestbucketrandom12345"
+  bucket_name       = "karthiktestbucketrandom12345"
   aws_s3_bucket_acl = "private"
 }
